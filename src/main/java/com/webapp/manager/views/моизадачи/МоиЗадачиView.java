1028b41ad3682dc9b7693b43822f68d334ca3cef
start.vaadin.com <no-reply@vaadin.com>
2024-07-10 12:48:19 +0000

    Generated project

---
+++ src/main/java/com/webapp/manager/views/моизадачи/МоиЗадачиView.java
@@ -0,0 +1,36 @@
+package com.webapp.manager.views.моизадачи;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+import com.webapp.manager.views.MainLayout;
+import jakarta.annotation.security.PermitAll;
+
+@PageTitle("Мои Задачи")
+@Route(value = "my-tasks", layout = MainLayout.class)
+@PermitAll
+public class МоиЗадачиView extends VerticalLayout {
+
+    public МоиЗадачиView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        H2 header = new H2("This place intentionally left empty");
+        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
+        add(header);
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
